853c9b271e14ebf05f721a1cc73b86b6a2088af0
wolfgarnet <wolfgarnet@gmail.com>
2016-01-06 13:17:36 +0100

    Metadata pt 7

--- walk/walker_test.go
+++ walk/walker_test.go
@@ -3,18 +3,48 @@ package walk
 import (
 	"testing"
 	"github.com/robertkrimen/otto/parser"
+	"github.com/robertkrimen/otto/ast"
+	"reflect"
 )
 
+type testWalker struct {
+	VisitorImpl
+	ancestors []Metadata
+}
+
+func (v *testWalker) VisitIdentifier(w *Walker, node *ast.Identifier, metadata []Metadata) {
+	v.ancestors = metadata
+}
+
 func TestWalker(t *testing.T) {
-
-	src := `function f() {a + b}`
-
-	program, err := parser.ParseFile(nil, "", src, 0)
-	if err != nil {
-		t.Errorf("Failed, %v", err)
+	tests := []struct{
+		src string
+		size int
+		parent reflect.Type
+	}{
+		{`1 + b`, 5, reflect.TypeOf((*ast.BinaryExpression)(nil))},
+		{`c++`, 5, reflect.TypeOf((*ast.UnaryExpression)(nil))},
 	}
 
-	visitor := &VisitorImpl{}
-	walker := Walker{visitor}
-	walker.Begin(program)
+	for i, test := range tests {
+		program, err := parser.ParseFile(nil, "", test.src, 0)
+		if err != nil {
+			t.Errorf("[%v] Failed, %v", i, err)
+		}
+
+		visitor := &testWalker{}
+		walker := Walker{visitor}
+		walker.Begin(program)
+
+		if test.size != len(visitor.ancestors) {
+			t.Errorf("[%v] Failed, number of ancestors not correct, %v != %v", i, test.size, len(visitor.ancestors))
+		}
+
+		parent := visitor.ancestors[len(visitor.ancestors)-2].Parent
+		typeOfParent := reflect.TypeOf(parent)
+
+		if test.parent != typeOfParent {
+			t.Errorf("[%v] Failed, parent not correct, %v != %v", i, test.parent, typeOfParent)
+		}
+	}
 }
