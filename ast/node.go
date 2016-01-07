780fe755a1871be48d1cf756fdbbef4d460a3c5f
wolfgarnet <wolfgarnet@gmail.com>
2016-01-07 09:01:31 +0100

    Removed function statement

--- ast/node.go
+++ ast/node.go
@@ -271,10 +271,6 @@ type (
 		Body        Statement
 	}
 
-	FunctionStatement struct {
-		Function *FunctionLiteral
-	}
-
 	IfStatement struct {
 		If         file.Idx
 		Test       Expression
@@ -343,7 +339,6 @@ func (*EmptyStatement) _statementNode()      {}
 func (*ExpressionStatement) _statementNode() {}
 func (*ForInStatement) _statementNode()      {}
 func (*ForStatement) _statementNode()        {}
-func (*FunctionStatement) _statementNode()   {}
 func (*IfStatement) _statementNode()         {}
 func (*LabelledStatement) _statementNode()   {}
 func (*ReturnStatement) _statementNode()     {}
@@ -428,7 +423,6 @@ func (self *EmptyStatement) Idx0() file.Idx      { return self.Semicolon }
 func (self *ExpressionStatement) Idx0() file.Idx { return self.Expression.Idx0() }
 func (self *ForInStatement) Idx0() file.Idx      { return self.For }
 func (self *ForStatement) Idx0() file.Idx        { return self.For }
-func (self *FunctionStatement) Idx0() file.Idx   { return self.Function.Idx0() }
 func (self *IfStatement) Idx0() file.Idx         { return self.If }
 func (self *LabelledStatement) Idx0() file.Idx   { return self.Label.Idx0() }
 func (self *Program) Idx0() file.Idx             { return self.Body[0].Idx0() }
@@ -487,7 +481,6 @@ func (self *EmptyStatement) Idx1() file.Idx      { return self.Semicolon + 1 }
 func (self *ExpressionStatement) Idx1() file.Idx { return self.Expression.Idx1() }
 func (self *ForInStatement) Idx1() file.Idx      { return self.Body.Idx1() }
 func (self *ForStatement) Idx1() file.Idx        { return self.Body.Idx1() }
-func (self *FunctionStatement) Idx1() file.Idx   { return self.Function.Idx1() }
 func (self *IfStatement) Idx1() file.Idx {
 	if self.Alternate != nil {
 		return self.Alternate.Idx1()
